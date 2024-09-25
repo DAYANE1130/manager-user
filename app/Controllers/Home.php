3f8ba7638899d468492308f37be004a0109acb5e
Dayane <dayanebmartins@gmail.com>
2024-09-25 14:18:53 -0300

    Testando conexão com banco de dados

---
+++ app/Controllers/Home.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Controllers;
+
+class Home extends BaseController
+{
+    public function index()
+    {
+        $db = db_connect();
+        $sql = 'select * from users';
+        $result = $db->query($sql);
+        var_dump($result->getResultObject());
+        return view('welcome_message');
+    }
+}
